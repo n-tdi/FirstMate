58bc5c297fbd1a1b776381b1c07c8f1a1156eef3
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-09-04 11:38:42 -0400

    hehehe

---
+++ src/main/java/world/ntdi/firstmate/FirstMate.java
@@ -0,0 +1,51 @@
+package world.ntdi.firstmate;
+
+import com.jagrosh.jdautilities.command.CommandClient;
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+
+public class FirstMate {
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        CommandClientBuilder builder = new CommandClientBuilder();
+        builder.forceGuildOnly("1011800940061401090");
+        builder.setServerInvite("https://discord.gg/Ahqeb8ahHn");
+        builder.setOwnerId("811580599068262421");
+        builder.setCoOwnerIds(348587937144897537L);
+        builder.setPrefix("/");
+        builder.useHelpBuilder(false);
+
+        builder.setActivity(Activity.watching("The Poop Deck"));
+        CommandClient commandClient = builder.build();
+
+        String token = getToken();
+
+        JDA jda = JDABuilder.createDefault(token)
+                .disableCache(CacheFlag.VOICE_STATE)
+                .setBulkDeleteSplittingEnabled(false)
+                .setChunkingFilter(ChunkingFilter.ALL)
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .addEventListeners(
+                        commandClient)
+                .build();
+
+        jda.awaitReady();
+        CommandListUpdateAction commandListUpdateAction = jda.updateCommands();
+        commandListUpdateAction.queue();
+    }
+
+    public static String getToken() {
+        Dotenv dotenv = Dotenv.load();
+        return dotenv.get("BOT_TOKEN");
+    }
+}
